9e6e48f18c040af1f50a3f6fc8c85d2f51518842
Thomas Sundberg <tsu@kth.se>
2015-10-30 10:48:30 +0100

    Read password from the environment

--- src/main/java/se/mtm/gradle/TriggerPuppetPlugin.java
+++ src/main/java/se/mtm/gradle/TriggerPuppetPlugin.java
@@ -8,6 +8,8 @@ import se.mtm.gradle.tasks.TriggerPuppetTask;
 import javax.inject.Inject;
 
 class TriggerPuppetPlugin implements Plugin<Project> {
+    private static final String ARTIFACTORY_PASSWORD = "ARTIFACTORY_PASSWORD";
+
     @Inject
     public TriggerPuppetPlugin() {
     }
@@ -15,6 +17,18 @@ class TriggerPuppetPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getExtensions().create("gradlePuppet", PluginDefaults.class);
+        getPassword();
         project.getTasks().create("triggerPuppet", TriggerPuppetTask.class);
+    }
+
+    private void getPassword() {
+        String password = System.getProperty("password");
+        if (password == null) {
+            password = System.getenv(ARTIFACTORY_PASSWORD);
+        }
+
+        if (password != null) {
+            System.setProperty("password", password);
+        }
     }
 }
